9b8d48a52fba6c62b527112abee8116e9814467a
Ruben Costa <daggosso2@gmail.com>
2022-05-29 17:40:20 +0100

    add absolute imports + fix tsconfig + add layout header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,20 @@
 import React, { FC, ReactNode } from 'react'
+import Navbar from './Navbar'
 
 interface LayoutProps {
   children: ReactNode
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <div>
+      <Navbar
+        title="Stars Wiki"
+        logoUrl="https://icon-library.com/images/star-wars-icon/star-wars-icon-9.jpg"
+      />
+      {children}
+    </div>
+  )
 }
 
 export default Layout
--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,12 +1,21 @@
 import React, { FC } from 'react'
 
 interface NavbarProps {
-  logoUrl: string
-  title: string
+  logoUrl?: string
+  title?: string
 }
 
-const Navbar: FC<NavbarProps> = () => {
-  return <div />
+const Navbar: FC<NavbarProps> = ({ logoUrl, title }) => {
+  return (
+    <header className="w-full block h-[64px] border-b">
+      <div className="mx-auto max-w-7xl w-full h-full">
+        <div className="flex items-center h-full">
+          <img className="h-full" alt="logo" src={logoUrl} />
+          {title && <span className="text-2xl font-bold">{title}</span>}
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
